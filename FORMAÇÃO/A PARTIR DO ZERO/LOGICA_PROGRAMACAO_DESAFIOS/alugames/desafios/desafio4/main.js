dcd5f9c707cb5d68125f4eb537dfe244a38fdac8
flaviacfagundes <flaviacrisfagundes@gmail.com>
2025-08-28 20:05:53 -0300

    challenge-four-completed
    Desafio 04 proposto pela Alura finalizado com sucesso.

--- FORMAÇÃO/A PARTIR DO ZERO/LOGICA_PROGRAMACAO_DESAFIOS/alugames/desafios/desafio4/main.js
+++ FORMAÇÃO/A PARTIR DO ZERO/LOGICA_PROGRAMACAO_DESAFIOS/alugames/desafios/desafio4/main.js
@@ -1,8 +1,19 @@
 
 function ordenarNumeros() {
-    let numberOne = document.getElementById('number-one');
-    let numberTwo = document.getElementById('number-two');
-    let numberThree = document.getElementById('number-three');
+    let numberOne = document.getElementById('number-one').value;
+    let numberTwo = document.getElementById('number-two').value;
+    let numberThree = document.getElementById('number-three').value;
 
+    if (numberOne == '' || numberTwo === '' || numberThree === '') {
+        alert('Necessário preencher todos os campos!');
+        return;
+    }
+
+    let numeros = [];
+    numeros.push(numberOne, numberTwo, numberThree);
+    let resultado = numeros.sort((a, b) => a - b);
+    alert(resultado);
     
 }
+
+// Fazer botão para reiniciar
