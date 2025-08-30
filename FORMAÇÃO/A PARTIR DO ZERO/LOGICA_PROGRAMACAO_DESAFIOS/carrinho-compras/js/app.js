9e8f93370a359fdbb4186be35368c1ee3ba04332
flaviacfagundes <flaviacrisfagundes@gmail.com>
2025-08-30 13:27:57 -0300

    carrinho-compras-project-completed
    Finalizei o projeto de carrinho de compras proposto pela Alura.

---
+++ FORMAÇÃO/A PARTIR DO ZERO/LOGICA_PROGRAMACAO_DESAFIOS/carrinho-compras/js/app.js
@@ -0,0 +1,60 @@
+
+function adicionar() {
+    let produtoAdicionado = document.getElementById('produto').value;
+    let quantidadeDeProduto = parseInt(document.getElementById('quantidade').value);
+
+    if (!quantidadeDeProduto) {
+        alert('Necessário informar a quantidade de produtos!');
+        return;
+    }
+
+    let valorProduto = parseInt(produtoAdicionado.split('R$')[1].trim());
+    let valorTotalProduto = valorProduto * quantidadeDeProduto;
+    
+    adicionarAoCarrinho(quantidadeDeProduto, produtoAdicionado, valorTotalProduto);
+    calcularTotal(valorTotalProduto);
+    document.getElementById('quantidade').value = '';
+}
+
+function adicionarAoCarrinho(quantidadeDeProduto, produtoAdicionado, valorTotalProduto) {
+    let produto = produtoAdicionado.split('-')[0].trim();
+
+    let novoItemCarrinho = document.createElement('section');
+    novoItemCarrinho.className = 'carrinho__produtos__produto';
+    novoItemCarrinho.innerHTML = `<span class="texto-azul">${quantidadeDeProduto}x</span> ${produto} <span class="texto-azul">R$${valorTotalProduto}</span>`;
+
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.appendChild(novoItemCarrinho);
+}
+
+let total = 0;
+
+function calcularTotal(valorTotalProduto) {
+    let somarProdutos = parseInt(total + valorTotalProduto);
+    total = somarProdutos;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.innerHTML = `R$ ${total}`;
+}
+
+function limpar() {
+    total = 0;
+    document.getElementById('valor-total').innerHTML = 'R$ 0';
+    let produtosCarrinho;
+    while (produtosCarrinho = document.querySelector('.carrinho__produtos__produto')){
+        produtosCarrinho.remove();
+    }
+    
+}
+
+// PEGAR VALOR DO PRODUTO
+
+//  O .split() separa a string em partes, a partir do parâmetro que você passa
+//  Entre conchetes fica o número 1 pois quero o valor que fica depois da sepração
+//  Se eu quisesse antes, é só colocar o 0 entre os conchetes
+//  E o .trim() serve para tirar espaços em branco
+
+// ADICIONAR ITEM NO CARRINHO
+
+//  appendChild() é um método JavaScript para manipular a DOM (Document Object Model) que insere um novo nó 
+//  (elemento ou nó de texto) como o último filho de um nó pai existente, permitindo a construção dinâmica de páginas web. 
+//  appendChild() adiciona um elemento à estrutura da página. 
