2fe14ddca2eddc6be66156904e94dc2692f788a5
flaviacfagundes <flaviacrisfagundes@gmail.com>
2025-09-01 14:30:55 -0300

    online-ticket-project-completed
    Projeto de venda de ingresso, proposto pela Alura, feito com sucesso.

---
+++ FORMAÇÃO/A PARTIR DO ZERO/LOGICA_PROGRAMACAO_DESAFIOS/ingresso/js/app.js
@@ -0,0 +1,46 @@
+let qtdPista = 100;
+let qtdSuperior = 200;
+let qtdInferior = 400;
+
+function comprar() {
+    let tipoIngresso = document.getElementById('tipo-ingresso').value;
+    let quantidade = parseInt(document.getElementById('qtd').value);
+    let pista = document.getElementById('qtd-pista');
+    let superior = document.getElementById('qtd-superior');
+    let inferior = document.getElementById('qtd-inferior');
+
+    if (!quantidade) {
+        alert('Necessário inserir a quantidade de ingressos!');
+        return;
+    }
+
+    if (tipoIngresso == 'pista') {
+        if (qtdPista < quantidade) {
+            alert('Quantidade indisponível para a venda na Pista');
+            return;
+        } else {
+            qtdPista = qtdPista - quantidade;
+            pista.innerHTML = qtdPista;
+        }
+    } else if (tipoIngresso == 'superior') {
+        if (qtdSuperior < quantidade) {
+            alert('Quantidade indisponível para a Cadeira Superior!');
+            return;  
+        } else {
+            qtdSuperior = qtdSuperior - quantidade;
+            superior.innerHTML = qtdSuperior;
+        }
+    } else if(tipoIngresso == 'inferior') {
+        if (qtdInferior < quantidade) {
+            alert('Quantidade indisponível para a Cadeira Inferior!');
+            return;           
+        } else {
+            qtdInferior = qtdInferior - quantidade;
+            inferior.innerHTML = qtdInferior;
+        }
+    } else {
+        alert('Deu ruim!');
+    }
+
+    document.getElementById('qtd').value = '';
+}
